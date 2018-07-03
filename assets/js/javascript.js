6e6b27d233dbbf2c75df85938335da8b5b93e163
tickstudiu <tickstudiu@gmail.com>
2018-07-03 14:57:57 +0700

    delete
    update system delete to website

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -18,6 +18,12 @@ const dbRoot = firebase.database().ref();
 
 // Create references
 const dbRefIncome = dbRoot.child('income');
+const dbRefInfo = dbRoot.child('info');
+var currentId = 0;
+
+dbRefInfo.on('value', function(snap){
+  currentId = snap.val().currentId;
+});
 
 dbRefIncome.on('value', function(snap){
 box.empty();
@@ -30,12 +36,15 @@ box.empty();
 
     snapshot.forEach(function(childSnapshot){
 
+      var key = childSnapshot.key;
+      console.log(key);
       var val = childSnapshot.val();
-
       var order = val.order;
       var price = val.price;
 
-      box.append("<tr><th scope='row'>"+ year + "-" + mouth + "-" +  day + "</th><td>" + order + "</td><td>" + price + "</td></tr>");
+      box.append("<tr><th scope='row'>"+ year + "-" + mouth + "-" +  day + "</th><td>"
+      + order + "</td><td>"
+      + price + "</td><td class='text-center'><button type='button' class='btn btn-outline-danger' onclick='handleDel("+year+","+mouth+","+day+","+key+")'>x</button></td></tr>");
 
     });
 
@@ -46,11 +55,36 @@ btnSubmit.on('click', function(){
   var inputDate = document.getElementById('date');
   var inputOrder = document.getElementById('order');
   var inputPrice = document.getElementById('price');
+  var orderId = currentId + 1;
 
-  dbRefIncome.child(inputDate.value).push({
+  var dbRef = dbRefIncome.child(inputDate.value);
+  dbRef.child(orderId).set({
     order: inputOrder.value,
     price: inputPrice.value
-  })
+  });
+
+  dbRefInfo.set({
+    currentId: orderId
+  });
 
   window.location.replace("https://tickstudiu.github.io/income-firebase-github-page/salary.html");
 });
+
+function handleDel(y , m , d, k) {
+  if (m < 10) {
+    m = '0' + m;
+  }
+
+  if (d < 10) {
+    d = '0' + d;
+  }
+
+  var path = 'income/' + y +'-'+ m +'-'+ d + '/' + k;
+  console.log(k);
+  var target = firebase.database().ref(path);
+  target.remove().then(function(){
+    console.log("remove done");
+  }).catch(function(error){
+    console.log("remove failed: " + error.message);
+  })
+}
